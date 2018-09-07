13be28532617a39f036c0c34b7d9066ca4b3f851
Orrin Lutes <onlutes@stu.naperville203.org>
2018-09-07 12:00:16 -0500

    Help

---
+++ Program1.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Program1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Program1
+{
+    public void String(ARGS)
+    {
+        System.out.print("Hey It's Ja Boi Pi");
+    }
+} 
